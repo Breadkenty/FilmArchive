830b786e12af1416b5fdfb5f75347ec8ff2a3353
Kento Kawakami <kawakamikento@gmail.com>
2020-10-22 19:31:08 -0400

    Added seed data for testing

--- FilmArchive.Persistence/Migrations/20201022230028_AddedListPhotosForEntries.cs
+++ FilmArchive.Persistence/Migrations/20201022230028_AddedListPhotosForEntries.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FilmArchive.Persistence.Migrations
 {
@@ -23,6 +24,31 @@ namespace FilmArchive.Persistence.Migrations
                 principalTable: "Entries",
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.InsertData(
+                table: "Entries",
+                columns: new string[] { "Title", "Date" },
+                values: new object[] { "Boston Day 1", Convert.ToDateTime("2019-10-26") });
+
+            migrationBuilder.InsertData(
+                table: "Entries",
+                columns: new string[] { "Title", "Date" },
+                values: new object[] { "Boston Day 2", Convert.ToDateTime("2019-10-27") });
+
+            migrationBuilder.InsertData(
+                table: "Entries",
+                columns: new string[] { "Title", "Date" },
+                values: new object[] { "Boston Day 3", Convert.ToDateTime("2019-10-28") });
+
+            migrationBuilder.InsertData(
+                table: "Entries",
+                columns: new string[] { "Title", "Date" },
+                values: new object[] { "Boston Day 4", Convert.ToDateTime("2019-10-29") });
+
+            migrationBuilder.InsertData(
+                table: "Entries",
+                columns: new string[] { "Title", "Date" },
+                values: new object[] { "Boston Day 5", Convert.ToDateTime("2019-10-30") });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
